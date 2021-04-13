07aec3b818e039e30e80bbda08f3d98a1fa09756
KienAnton <78024848+KienAnton@users.noreply.github.com>
2021-04-13 19:00:46 +0700

    update

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 //console.log('PATH: ',path.join(__dirname, 'resources/views'));
-app.get('/trang-tin', (req, res) => {
+app.get('/', (req, res) => {
   return res.render('home');
 });
 
